47dc4bd064f770f49c35d6306220f5a178d9a26b
AlexTLDR <alex.badragan@protonmail.com>
2025-05-09 19:58:13 +0200

    refactoring for login command

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,57 +3,50 @@ package config
 import (
 	"encoding/json"
 	"os"
-	"path/filepath"
 )
 
-// This is only intended for local use so
-// I am not hiding the credentials
 const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	DBURL           string `json:"db_url"`
-	CurrentUserName string `json:"current_user_name,omitempty"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
-func getConfigFilePath() (string, error) {
-
-	return filepath.Join("./", configFileName), nil
+func (cfg *Config) SetUser(userName string) error {
+	cfg.CurrentUserName = userName
+	return write(*cfg)
 }
 
 func Read() (Config, error) {
-	var cfg Config
-
-	path, err := getConfigFilePath()
+	file, err := os.Open(configFileName)
 	if err != nil {
-		return cfg, err
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return Config{}, err
 	}
 
-	data, err := os.ReadFile(path)
-	if err != nil {
-		return cfg, err
-	}
-
-	err = json.Unmarshal(data, &cfg)
-	return cfg, err
+	return cfg, nil
 }
 
-// write writes the config struct to the config file
 func write(cfg Config) error {
-	path, err := getConfigFilePath()
+
+	file, err := os.Create(configFileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(cfg)
 	if err != nil {
 		return err
 	}
 
-	data, err := json.Marshal(cfg)
-	if err != nil {
-		return err
-	}
-
-	return os.WriteFile(path, data, 0644)
-}
-
-// SetUser sets the current user and updates the config file
-func (cfg *Config) SetUser(username string) error {
-	cfg.CurrentUserName = username
-	return write(*cfg)
+	return nil
 }
--- main.go
+++ main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"gator/internal/config"
 	"log"
+	"os"
 )
 
-func main() {
+type state struct {
+	cfg *config.Config
+}
 
+func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	err = cfg.SetUser("Alex")
+	programState := &state{
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
+
+	cmdName := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
 	if err != nil {
-		log.Fatalf("Failed to set user: %v", err)
+		log.Fatal(err)
 	}
-
-	updatedCfg, err := config.Read()
-	if err != nil {
-		log.Fatalf("Failed to reload configuration: %v", err)
-	}
-
-	fmt.Printf("%+v\n", updatedCfg)
 }
